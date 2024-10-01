ef8cbe2ff871d1b36f99a4c214a0d91099dda870
Vushie <skieu794@gmail.com>
2024-10-01 05:51:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def get_matcha():
     return jsonify(matcha_stock)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 1000))
+    app.run(host='0.0.0.0', port=port, debug=True)
