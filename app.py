e55711e28617cbb3d62ab09629916d4423cead40
Vushie <skieu794@gmail.com>
2024-10-01 08:46:09 -0700

    schedule and display

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from MatchaScript import scrape_matchas
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask('Matcha_Watch')
 
@@ -8,7 +9,9 @@ app = Flask('Matcha_Watch')
 def get_matcha():
     matcha_stock = scrape_matchas()
     return jsonify(matcha_stock)
-
+scheduler = BackgroundScheduler()
+scheduler.add_job(scrape_data, 'interval', minutes=1)
+scheduler.start()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
