20b389dd7a145bd7b85ccc9e148b3a319608f53c
Vushie <skieu794@gmail.com>
2024-10-08 12:33:56 -0700

    removed scheduling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from MatchaScript import scrape_matchas
-from apscheduler.schedulers.background import BackgroundScheduler
+
 
 app = Flask('Matcha_Watch')
 CORS(app ,origins=["https://stkieu.github.io"])
@@ -11,9 +11,7 @@ CORS(app ,origins=["https://stkieu.github.io"])
 def get_matcha():
     matcha_stock = scrape_matchas()
     return jsonify(matcha_stock)
-scheduler = BackgroundScheduler()
-scheduler.add_job(scrape_matchas, 'interval', minutes=1)
-scheduler.start()
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
