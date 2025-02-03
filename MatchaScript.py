b8d072d3e9cf7aab03f96fc4a97059ef2e9fea8d
Vushie <skieu794@gmail.com>
2025-02-03 01:35:25 -0800

    updated to scrape sazen instead

--- MatchaScript.py
+++ MatchaScript.py
@@ -4,23 +4,34 @@ import pandas as pd
 
 def scrape_matchas():
     # want the site with the principal matchas
-    webpage_response = requests.get("https://www.marukyu-koyamaen.co.jp/english/shop/products/catalog/matcha/principal")
+    webpage_response = requests.get("https://www.sazentea.com/en/products/c24-marukyu-koyamaen-matcha")
     webpage = webpage_response.content
     soup = BeautifulSoup(webpage, "html.parser")
 
     #get the ul with the matchas
-    matcha_types = soup.find(class_="products")
+    matcha_types = soup.find(class_="principal") #should be the table
+    type_rows = matcha_types.findAll('tr')
     matcha_stock = {}
 
-    # each list item
-    for matcha in matcha_types.children:
-        matcha_name = matcha.find(class_="product-name").h4.text
-        matcha_url = matcha.find(class_="woocommerce-loop-product__link")['href']
-        classes = matcha.get('class', [])
-        # MK uses classes to classify stock
-        if 'outofstock' in classes:
-            matcha_stock[matcha_name] = {"stock": "0", "url" : matcha_url}
-        elif 'instock' in classes:
+    base_url = "https://www.sazentea.com"
+    # each table item
+    for matcha in type_rows[1:]:
+        matcha_data = matcha.findAll('td')
+        matcha_name = matcha_data[1].text.strip()
+        matcha_url = base_url+matcha_data[1].find('a')['href']
+
+        web = requests.get(matcha_url)
+        webContent = BeautifulSoup(web.content, "html.parser")
+        info = webContent.find('strong', class_='red')
+        
+        if info:
+            # Sazen uses strong tag to classify stock
+            matcha_stock[matcha_name] = {"stock": "0", "url" : matcha_url}  
+        else:
             matcha_stock[matcha_name] = {"stock": "1", "url" : matcha_url}
     
     return matcha_stock
+
+if __name__ == "__main__":
+    matcha_stock = scrape_matchas()
+    print(matcha_stock)
